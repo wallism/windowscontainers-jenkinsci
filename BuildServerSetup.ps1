7c0c46ae89908d1345c0dd482c9e613e22120f0c
wallism <mark@agiledevelopment.com.au>
2017-03-01 06:34:03 +0000

    added (move later)

---
+++ BuildServerSetup.ps1
@@ -0,0 +1,25 @@
+﻿<#
+Extra server setup steps
+
+
+7. Change Dockerfile in master and agent to use: nano-java:jre1.8.0_91 
+#>
+
+cd c:\
+mkdir jenkins
+cd jenkins
+
+# Installs: 
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+cinst -y git
+cinst -y notepadplusplus
+cinst -y jdk8nn
+
+& 'C:\Program Files\Git\bin\git' clone https://github.com/wallism/windowscontainers-jenkinsci.git
+
+cd .\windowscontainers-jenkinsci\java_jre
+
+docker build -t nano-java:jre1.8.0_91 .
+
+
